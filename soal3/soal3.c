5203ab24779c2e0446af4033b6ebe842aac50411
Muhammad Fadhlan Min Robby <fadhlan.mr@gmail.com>
2019-04-13 08:03:41 +0700

    hasta revision

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -6,34 +6,58 @@
 
 int WakeUp_Status;
 int Spirit_Status;
+int statusa;
+int statusi;
 pthread_t tid[777];
 
 void* Agmal(void* arg){
     int num = *((int *) arg);
-    if(num == 1){
-        printf("Agmal WakeUp_Status = %d\n", WakeUp_Status);
-    }
-    else if(num == 2){
-        WakeUp_Status += 15;
-        printf("Agmal mulai bangun\n");
-    }
+    //while(1){
+        if(statusa==1){
+            sleep(10);
+            statusa = 0;
+            //break;
+        }
+        else{
+            if(num == 1){
+                printf("Agmal WakeUp_Status = %d\n", WakeUp_Status);
+                //break;
+            }
+            else if(num == 2){
+                WakeUp_Status += 15;
+                printf("Agmal mulai bangun\n");
+                //break;
+            }
+        }
+    //}
 }
 
 void* Iraj(void* arg){
     int num = *((int *) arg);
-    if(num == 1){
-        printf("Iraj Spirit_Status = %d\n", Spirit_Status);
-    }
-    else if(num == 3){
-        Spirit_Status -= 20;
-        printf("Iraj mulai ngantuk\n");
-    }
+    //while(1){
+        if(statusi==1){
+            sleep(10);
+            statusi = 0;
+            //break;
+        }
+        else{
+            if(num == 1){
+                printf("Iraj Spirit_Status = %d\n", Spirit_Status);
+                //break;
+            }
+            else if(num == 3){
+                Spirit_Status -= 20;
+                printf("Iraj mulai ngantuk\n");
+                //break;
+            }
+        }
+    //}
 }
 
 int main(void){
     WakeUp_Status = 0;
     Spirit_Status = 100;
-    int i[777],temp,countW,countS;
+    int i[777],temp,countW=0,countS=0;
     int j=1,ja=1;
     char read[77];
     int *p=&temp;
@@ -45,22 +69,30 @@ int main(void){
         }
         else if (strcmp(read,"Agmal Ayo Bangun")==10){
             if(countS==3){
-                printf("Agmal Ayo Bangun disabled 10 s");
+                temp=0;
+                statusa=1;
                 countS=0;
             }
             else{
                 temp = 2;
-                countW +=1;   
+                countW +=1;
+                if(countW==3){
+                    printf("Iraj Ayo Tidur disabled 10 s\n");
+                }   
             }
         }
         else if (strcmp(read,"Iraj Ayo Tidur")==10){
             if(countW==3){
-                printf("Iraj Ayo Tidur disabled 10 s");
+                temp=0;
+                statusi=1;
                 countW=0;
             }
             else{
                 temp = 3;
                 countS +=1;   
+                if(countS==3){
+                    printf("Agmal Ayo Bangun disabled 10 s\n");
+                }
             }
         }
         //printf("temp = %d, countW = %d, countS = %d, j = %d, &p = %d, read = %s\n", temp, countW, countS, j, *p, read);
@@ -83,11 +115,11 @@ int main(void){
         pthread_join(tid[ja], NULL);
         pthread_join(tid[ja+1], NULL);
         j+=2;
-        if(WakeUp_Status == 100){
+        if(WakeUp_Status >= 100){
             printf("Agmal Terbangun, mereka bangun pagi dan berolahraga\n");
             break;
         }
-        else if(Spirit_Status == 0){
+        if(Spirit_Status <= 0){
             printf("Iraj ikut tidur, dan bangun kesiangan bersama Agmal\n");
             break;
         }
